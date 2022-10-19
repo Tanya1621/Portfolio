62a4a49a59f25432db66e5be4542f79c85847498
Tanya1621 <70881555+Tanya1621@users.noreply.github.com>
2022-10-19 14:20:30 +0300

    fixed light theme

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export const Footer = ({theme, setTheme}) => {
     const switchTheme = (e) => {
         const switcher = document.querySelector(`.${style.switch}`);
         theme === 'dark'? setTheme('light') : setTheme('dark');
-        switcher.style.justifyContent === 'flex-start' || '' ? switcher.style.justifyContent = 'flex-end' : switcher.style.justifyContent = 'flex-start';
+        switcher.style.justifyContent === 'flex-start' || switcher.style.justifyContent === '' ? switcher.style.justifyContent = 'flex-end' : switcher.style.justifyContent = 'flex-start';
 
     }
     return (
